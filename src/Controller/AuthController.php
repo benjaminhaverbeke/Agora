60be01c4d7f9b639bb1b12a30f159b22b9f5e682
benjaminhaverbeke <benjamin.haverbeke@outlook.com>
2024-07-09 16:25:58 +0200

    authrouting init

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('auth/login.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {
+        return $this->render('auth/register.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+        session_destroy();
+
+        header('Location: index.php');
+    }
+
+}
